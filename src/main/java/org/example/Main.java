36d8804b3ffdbe8e354e1e1009a4e7ffabbeaeb8
ilyab <ilyabeskrovny@mail.ru>
2023-04-15 18:56:17 +0400

    польская бесскобочная запись

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,12 @@
 package org.example;
 
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Calculation res = new Calculation();
-        String str = "323+*";
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите выражение в постфиксной форме: ");
+        String str = in.nextLine();
         System.out.println(res.calc(str));
     }
 }
